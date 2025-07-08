ce472d0d86c2849d4e05d15bf131a67d5f60c814
OmNagre04 <omnagare04@gmail.com>
2025-07-08 10:59:27 +0530

    Fonts, Assests & Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 const Home = () => {
   return (
     <div>
-      <h1>Found you home page❤️</h1>
+      <h1 className="text-2xl">Found you home page ❤️</h1>
+      <h1 className="font-space-grotesk text-2xl">Found you home page ❤️</h1>
     </div>
-  )
-}
+  );
+};
+
 
 export default Home
